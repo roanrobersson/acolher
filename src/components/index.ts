2bbde6b09eb6e35b930ba2cdb204aad029eceb60
Roan de Oliveira <roanrobersson@gmail.com>
2024-07-01 17:30:41 -0300

    feat: :sparkles: add IconButton component

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,7 @@ export * from "./Dialog";
 export * from "./Drawer";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
+export { default as IconButton } from "./IconButton";
 export { default as Input } from "./Input";
 export { default as Link } from "./Link";
 export * from "./Modal";
