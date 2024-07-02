c647c6878afaac902651e54b723ba0cd92bb9b10
Roan de Oliveira <roanrobersson@gmail.com>
2024-07-02 10:51:36 -0300

    refactor: :recycle: remove unused hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,3 @@
-export { default as useBreakpointValue } from "./useBreakpointValue";
-export { default as useBreakpoints } from "./useBreakpoints";
 export { default as useCombinedRefs } from "./useCombinedRefs";
 export { default as useDarkMode } from "./useDarkMode";
-export { default as useScreenResized } from "./useScreenResized";
 export { default as useSticky } from "./useSticky";
